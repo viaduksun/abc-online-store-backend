8d8ed6dba995abc9628c1e22f1b486ff9c1821e1
Vadim Budko <viaduksun@gmail.com>
2021-08-05 19:33:21 +0300

    public

--- server.js
+++ server.js
@@ -76,7 +76,8 @@ app.use('/', mainRoute);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
+  app.use(express.static(__dirname + '/public'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
